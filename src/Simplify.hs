e972345c0d1b650e776a4ff0fff32ad56167d49f
Walter Schulze <awalterschulze@gmail.com>
2017-01-07 15:51:41 +0100

    moved simplifyValue

--- src/Simplify.hs
+++ src/Simplify.hs
@@ -112,36 +112,3 @@ checkRef refs p = case reverseLookupRef p refs of
 	Nothing  	-> p
 	(Just k) 	-> Reference k
 
-simplifyValue :: BoolExpr -> BoolExpr
-simplifyValue e@(EqualFunc _) = e
-simplifyValue (OrValue v1 v2) = simplifyOrValue (simplifyValue v1) (simplifyValue v2)
-simplifyValue (AndValue v1 v2) = simplifyAndValue (simplifyValue v1) (simplifyValue v2)
-simplifyValue (NotValue v) = simplifyNotValue (simplifyValue v)
-simplifyValue AnyValue = AnyValue
-
-simplifyOrValue :: BoolExpr -> BoolExpr -> BoolExpr
-simplifyOrValue AnyValue _ = AnyValue
-simplifyOrValue _ AnyValue = AnyValue
-simplifyOrValue (NotValue AnyValue) v = v
-simplifyOrValue v (NotValue AnyValue) = v
-simplifyOrValue v1 v2
-	| v1 == v2  = v1
-	| v1 == (NotValue v2) = AnyValue
-	| (NotValue v1) == v2 = AnyValue
-	| otherwise = OrValue v1 v2
-
-simplifyAndValue :: BoolExpr -> BoolExpr -> BoolExpr
-simplifyAndValue AnyValue v = v
-simplifyAndValue v AnyValue = v
-simplifyAndValue (NotValue AnyValue) _ = NotValue AnyValue
-simplifyAndValue _ (NotValue AnyValue) = NotValue AnyValue
-simplifyAndValue v@(EqualFunc v1) (EqualFunc v2)
-	| v1 == v2  = v
-	| otherwise = (NotValue AnyValue)
-simplifyAndValue v1 v2
-	| v1 == v2  = v1
-	| otherwise = AndValue v1 v2
-
-simplifyNotValue :: BoolExpr -> BoolExpr
-simplifyNotValue (NotValue v) = v
-simplifyNotValue v = NotValue v
