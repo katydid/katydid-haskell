e713210f1b932a05d9eb8c2b50409fb05a6fb18c
Walter Schulze <awalterschulze@gmail.com>
2017-01-13 09:35:25 +0100

    parseint for xml

--- src/Xml.hs
+++ src/Xml.hs
@@ -1,6 +1,8 @@
 {-# LANGUAGE FlexibleInstances #-}
 
 module Xml where
+import Text.Read (readMaybe)
+import Debug.Trace
 
 import Text.XML.HXT.DOM.TypeDefs (XmlTree, XNode(..), blobToString, localPart)
 import Text.XML.HXT.Parser.XmlParsec (xread)
@@ -16,16 +18,20 @@ decodeXML :: String -> [XmlTree]
 decodeXML = xread
 
 xmlLabel :: XNode -> Label
-xmlLabel (XText s) = String s
-xmlLabel (XBlob b) = String $ blobToString b
+xmlLabel (XText s) = parseLabel s
+xmlLabel (XBlob b) = parseLabel $ blobToString b
 xmlLabel x@(XCharRef _) = error $ "XCharRef not supported" ++ show x
 xmlLabel x@(XEntityRef _) = error $ "XEntityRef not supported" ++ show x
 xmlLabel x@(XCmt _) = error $ "XCmt not supported" ++ show x
-xmlLabel (XCdata s) = String s
+xmlLabel (XCdata s) = parseLabel s
 xmlLabel x@(XPi _ _) = error $ "XPi not supported" ++ show x
-xmlLabel (XTag qname attrs) = String (localPart qname) -- TODO attrs should be part of the children returned by getChildren
+xmlLabel (XTag qname attrs) = parseLabel (localPart qname) -- TODO attrs should be part of the children returned by getChildren
 xmlLabel x@(XDTD _ _) = error $ "XDTD not supported" ++ show x
-xmlLabel (XAttr qname) = String (localPart qname)
+xmlLabel (XAttr qname) = parseLabel (localPart qname)
 xmlLabel x@(XError _ _) = error $ "XError not supported" ++ show x
 
-
+-- TODO what about other leaf types
+parseLabel :: String -> Label
+parseLabel s = case (readMaybe s :: Maybe Int) of
+	(Just i) -> Number (toRational i)
+	Nothing -> String s
